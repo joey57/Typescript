495c8f805f69aa82f57cc4e850cdff2fdb2a2694
joey57 <njorogejoey5@gmail.com>
2022-03-31 11:30:02 +0300

    comment array destructing syntax

--- var-sub.js
+++ var-sub.js
@@ -1,3 +1,7 @@
 const name = 'James Muriuki'
 let introduction = `Hi, my name is ${name}`
 console.log(introduction);
+//Array Destructing
+// const num = [1, 2]
+// const[a,b]=num
+// console.log(a);
