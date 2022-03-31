ad4da4f76dc1588ff954160069299af5f502b249
joey57 <njorogejoey5@gmail.com>
2022-03-31 13:21:34 +0300

    added then function

--- promise.js
+++ promise.js
@@ -3,7 +3,7 @@ function asycTask(){
     setTimeout(()=>{
       try{
       console.log("Action Complete");
-      resolve();//we call the resolve function ater the async had been completed.
+      resolve("Success");//we call the resolve function ater the async had been completed.//pass value to resolve function.
     }catch(err){
       reject()//when we encouter an error we call the reject function.
     }
@@ -11,3 +11,8 @@ function asycTask(){
   });
   return promise;
 }
+asycTask().then(()=>{console.log(val)},()=>{console.log(err)},)
+//then method takes on 2 anonymous functions as arguments the first one is called if the asyntask is successful the other when the task fails.
+//then function alerts us whether our asyntask is complete or not.
+//val and err values of resolve and reject are used by the then function argument.
+//we place the values inside the resolve and reject function and log them when we call then function.
